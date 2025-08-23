0e7a6318b34788e238c4e34fdf52edc2b5a5ceb7
Kevin Ramos <keramosl@unal.edu.co>
2025-08-22 22:20:32 -0500

    Implementación del entrenamiento del baseline

--- biomedical-classification/src/train.py
+++ biomedical-classification/src/train.py
@@ -1,1 +1,15 @@
 # Entrenar Modelos
+from sklearn.linear_model import LogisticRegression
+from sklearn.multiclass import OneVsRestClassifier
+import joblib
+
+def baseline_train(X_train, y_train):
+  modelo = OneVsRestClassifier(LogisticRegression(max_iter=200))
+  modelo.fit(X_train, y_train)
+  return modelo
+
+def guardar_modelo(modelo, ruta):
+  joblib.dump(modelo,ruta)
+
+def cargar_modelo(ruta):
+  return joblib.load(ruta)
