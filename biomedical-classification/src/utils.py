1145f78f983fa7e25a16375aaf1dc0081edf05a2
Kevin Ramos <keramosl@unal.edu.co>
2025-08-22 22:39:48 -0500

    Implementación de utils.py

--- biomedical-classification/src/utils.py
+++ biomedical-classification/src/utils.py
@@ -1,1 +1,13 @@
 # Funciones auxiliares
+import numpy as np  
+import random
+import torch  
+
+def semilla(seed=42):
+  # Se fija la semilla para reproducibilidad en numpy, random y torch
+  
+  random.seed(seed)
+  np.random.seed(seed)
+  if torch.cuda.is_available():
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
